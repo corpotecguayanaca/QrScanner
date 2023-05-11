74d5e92b524fa0d6a73dd9d52e46e5f3a919c554
Luis Marquez <luisjose0527@gmail.com>
2023-05-11 14:09:08 -0400

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:qrscannerapp/src/pages/direcciones_page.dart';
+import 'package:qrscannerapp/src/pages/home_page.dart';
+import 'package:qrscannerapp/src/pages/mapas_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QRScanner',
+      initialRoute: 'home',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        'home': (context) => HomePage(),
+        'mapas': (context) => MapasPage(),
+        'direcciones':(context) => DireccionesPage(),
+      },
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
+      ),
+    );
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:qrscannerapp/src/pages/direcciones_page.dart';
+import 'package:qrscannerapp/src/pages/mapas_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QRScanner"),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        titleTextStyle: TextStyle( 
+          color: Theme.of(context).colorScheme.onPrimary,
+          fontSize: Theme.of(context).textTheme.headlineLarge!.fontSize,
+        ),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.delete_forever, color: Theme.of(context).colorScheme.onPrimary,))
+        ],
+      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _createBottomNAvigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.filter_center_focus),
+      ),
+    );
+  }
+
+  Widget _createBottomNAvigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index){
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: "Mapas",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.computer),
+          label: "Direcciones",
+        ),
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch(paginaActual) {
+      case 0: return MapasPage();
+      case 1: return DireccionesPage();
+      default: return MapasPage();
+    }
+  }
+}
